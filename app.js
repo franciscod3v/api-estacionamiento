27ff58171e0f834467cc0ae93eb7c0f2e0ce8253
Franciscod3v <franciscod3v@gmail.com>
2023-10-22 20:11:21 -0500

    Se agrega cors a archivo app.js

--- app.js
+++ app.js
@@ -2,12 +2,17 @@ import express from 'express'
 
 const app = express()
 
+import { corsMiddleWare } from './middlewares/cors.js'
+
 const PORT = process.env.PORT ?? 1235
 
 app.disable('x-powered-by')
 
 //Middleware
 app.use(express.json())
+
+//Cors
+app.use(corsMiddleWare())
 
 
 app.listen(PORT, () => { 
