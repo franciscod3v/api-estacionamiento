b7f524f7486df7cc6b3fd9689a125f34da1c9fb7
Franciscod3v <franciscod3v@gmail.com>
2023-10-22 23:43:51 -0500

    Se termina clase Registro.js sin agrega delete

--- models/Registro.js
+++ models/Registro.js
@@ -6,8 +6,54 @@ import { randomUUID } from 'node:crypto'
 
 export class Registro {
 
-    static async getAll () {
-        
+    static async getAll({ placa }) {
+        let registrosFiltrados = registrosJSON
+        if (placa) {
+            registrosFiltrados = registrosFiltrados.filter(
+                registro => registro.placa.toLowerCase() === placa.toLowerCase()
+            )
+        }
+        return registrosFiltrados
+    }
+
+    static async getById({ id }) {
+        const registro = registrosJSON.find(
+            registro => registro.id === id
+        )
+
+        return registro
+    }
+
+    static async create({ input }) {
+        //Creando el registro
+        const cantidadDeRegistros = registrosJSON.length
+        const ultimoId = registrosJSON[cantidadDeRegistros - 1].id
+        const nuevoRegistro = {
+            id: ultimoId + 1,
+            ...input
+        }
+        //Agregando nuevo registro
+        registrosJSON.push(nuevoRegistro)
+        return nuevoRegistro
+
+    }
+
+    static async update({ id, input }) {
+        //Buscamos id en arreglo
+        const registroId = registrosJSON.findIndex(registro => registro.id === id)
+        //Verificando si se encontró el Id
+        if (registroId === -1) {
+            return { valor: false, message: 'Registro no encontrado'}
+        }
+        //Creando el registro con el Id encontrado
+        const registroActualizado = {
+            ...registrosJSON[registroId],
+            ...input
+        }
+        //Reemplazamos el registro actualizado por el registro antiguo
+        registrosJSON[registroId] = registroActualizado
+
+        return{valor: true, message: 'Registro actualizado'}
     }
 
 }
