c5ac931a4b0822f8b7ebe6a5423ebf89c14b7a47
Franciscod3v <franciscod3v@gmail.com>
2023-10-23 00:04:56 -0500

    Se agrega routes en app.js

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const app = express()
 
 import { corsMiddleWare } from './middlewares/cors.js'
 
+import { router } from './routes/registros.js'
+
 const PORT = process.env.PORT ?? 1235
 
 app.disable('x-powered-by')
@@ -13,6 +15,9 @@ app.use(express.json())
 
 //Cors
 app.use(corsMiddleWare())
+
+//Routes
+app.use('/registros', router)
 
 
 app.listen(PORT, () => { 
